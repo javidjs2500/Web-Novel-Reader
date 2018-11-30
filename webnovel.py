ff30a6c035672fef5b9cfb31a115cf5e1fe3bab1
Javid Singh <javidjs@uci.edu>
2018-11-29 20:55:03 -0800

    made read function

--- webnovel.py
+++ webnovel.py
@@ -1,25 +1,75 @@
 # Work with Python 3.6
-import discord
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
+from discord import ChannelType
+
 BOT_PREFIX = ("?", "!")
 TOKEN = 'NTE3ODgzMjA0NDgxNTgxMDYw.DuIsLA.61SIijbhN9Uxw8SpBR9HbeZAgAI'
 
-client = discord.Client()
+client = Bot(command_prefix=BOT_PREFIX)
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    await client.change_presence(game=Game(name="with humans"))
+    print("Logged in as " + client.user.name)
 
+
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
+
+@client.command(pass_context = True)
+@asyncio.coroutine
+async def read(ctx):
+    print(ctx.message)
+    print(ctx.message.author)
+    print(ctx.message.channel.type)
+
+    if (ctx.message.channel.type != ChannelType.private):
+        await client.say("Sorry, this command cannot be used here. Please message me privately!")
+    else:
+        await client.say("Let's start!")
+    
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+
+
+client.loop.create_task(list_servers())
 client.run(TOKEN)
